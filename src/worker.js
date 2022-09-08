e0d939f641d31d99d20b04f657d966e751918bb0
anurajguha <anurajguha@gmail.com>
2022-09-08 21:58:39 +0530

    update worker

--- src/worker.js
+++ src/worker.js
@@ -3,14 +3,18 @@ const { messageQueue, queueName } = require("./config/vars");
 const consumeAndForward = async (io) => {
     const channel = messageQueue.channel;
     channel.consume(queueName, (msg) => {
-        if (msg) {
-            console.log('message recieved: ', msg.content.toString());
-            io.emit('new_message', msg.content.toString());
-            channel.ack(msg);
-        } else {
-            console.log('Consumer cancelled by server');
+        try {
+            if (msg) {
+                console.log('message recieved: ', msg.content.toString());
+                io.emit('new_message', msg.content.toString());
+                channel.ack(msg);
+            } else {
+                console.log('Consumer cancelled by server');
+            }
+        } catch (error) {
+            console.log('something went wrong ', error.message);
         }
-    }, { noAck: false });
+    });
 };
 
 module.exports = { consumeAndForward };
